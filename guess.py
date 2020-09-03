3268b2770d8769e4b4d7499b8284fb0c3616afef
eki66 <soshieki@gmail.com>
2020-09-03 11:38:13 +0800

    final version

--- guess.py
+++ guess.py
@@ -1,8 +1,14 @@
-# 猜數字遊戲,並顯示與答案的大小關係及猜的次數
+# 猜數字遊戲
+# 讓使用者輸入隨機數字的範圍
+# 並顯示與答案的大小關係及猜的次數
 
 import random
+start = input('請決定隨機數字範圍開始值:')
+end = input('請決定隨機數字範圍結束值:')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 100)
+r = random.randint(start, end)
 count = 0
 while True:
 	count += 1 # count = count + 1
